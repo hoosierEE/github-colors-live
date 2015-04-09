50ea99885e6e0b744afd77ebf7e47c4948fe57cb
hoosierEE <ashroyer@gmail.com>
2015-04-09 15:08:00 -0400

    remove testing comment

--- Rebase.elm
+++ Rebase.elm
@@ -3,8 +3,6 @@ module Rebase (decimalFromHex) where
 
 import String (indexes,fromChar,toList)
 import List (map,head,reverse,indexedMap,foldr)
--- import Text (asText)
--- main = asText <| decimalFromHex "010"
 
 decimalFromHex : String -> Int
 decimalFromHex hexString =
@@ -12,3 +10,4 @@ decimalFromHex hexString =
         vals = map (head << flip indexes "0123456789ABCDEF0123456789abcdef" << fromChar) xs
         powered = indexedMap (\idx val -> (16 ^ idx) * (val % 16)) vals
     in foldr (+) 0 powered
+
