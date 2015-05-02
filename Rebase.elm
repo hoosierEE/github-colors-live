629341013359821c601d5869acd973e5df9bacac
hoosierEE <ashroyer@gmail.com>
2015-05-01 20:29:33 -0400

    tiny comment

--- Rebase.elm
+++ Rebase.elm
@@ -6,7 +6,8 @@ charset_hex = "0123456789ABCDEF0123456789abcdef"
 decFromHex hexString =
     let
         ss = List.reverse <| S.toList hexString
-        vals = List.filterMap (List.head << flip S.indexes charset_hex << S.fromChar) ss
+        vals = List.filterMap -- omits Nothings
+            (List.head << flip S.indexes charset_hex << S.fromChar) ss
         pwrd = List.indexedMap (\x v -> (16^x)*(v%16)) vals
     in List.foldr (+) 0 pwrd
 
